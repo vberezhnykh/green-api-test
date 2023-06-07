f80d9361b62bbdf51365d70c93d8d430933cdceb
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-07 22:59:33 +0300

    feat: add send ico to chat input; message can be send via enter key

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -56,6 +56,11 @@ const Chat = () => {
     chatInputRef.current.value = "";
   };
 
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter") return;
+    handleClick();
+  };
+
   return (
     <section className="chat">
       <header className="chat__header">
@@ -66,8 +71,29 @@ const Chat = () => {
         <Messages />
       </div>
       <div className="chat__input-container">
-        <input name="chat" className="chat__input" ref={chatInputRef} />
-        <button onClick={handleClick}>Send</button>
+        <input
+          name="chat"
+          className="chat__input"
+          ref={chatInputRef}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleClick} className="chat__send-button">
+          <svg
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            preserveAspectRatio="xMidYMid meet"
+            version="1.1"
+            x="0px"
+            y="0px"
+            enableBackground="new 0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M1.101,21.757L23.8,12.028L1.101,2.3l0.011,7.912l13.623,1.816L1.112,13.845 L1.101,21.757z"
+            ></path>
+          </svg>
+        </button>
       </div>
     </section>
   );
