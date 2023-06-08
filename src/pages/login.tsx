d7ad21c92fb53e29195568a02d9d466ee76ab39c
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-08 22:46:10 +0300

    feat: add header

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,14 @@
 import Footer from "../components/footer";
 import LoginForm from "../components/login_form";
+import whatsappImgSrc from "../assets/whatsapp.png";
 
 const Login = () => {
   return (
     <>
+      <header className="header">
+        <img src={whatsappImgSrc} alt="" />
+        <h1>Green Api</h1>
+      </header>
       <main className="main">
         <LoginForm />
       </main>
